0b6945e94e1006fe9be2aa9fbde276359ab4b041
Anton Jouline <juce66@gmail.com>
2024-07-30 15:10:09 -0700

    small update for swiftbar script

--- totp-swiftbar.sh
+++ totp-swiftbar.sh
@@ -11,5 +11,6 @@ cat $1 | ${HOME}/bin/totp | pbcopy
 echo TOTP
 echo ---
 echo "Okta| bash=$0 param1=${HOME}/.ssh/okta_key.txt terminal=false"
+echo "Github| bash=$0 param1=${HOME}/.ssh/github_key.txt terminal=false"
 
-# add more echo lines ^^^ for other keys
+# add more echo lines like that ^^^ - for other keys, as needed
